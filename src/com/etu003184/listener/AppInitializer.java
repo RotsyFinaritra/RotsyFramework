94d24a60f0bd248aab8d050da2fa7eb15ccca853
Randriamifidisoa Rotsy Finaritra <rotsyfinaritra0@gmail.com>
2025-11-09 13:31:41 +0300

    Creation de AppInitializer pour que le scan se fasse lors du deploiement de l'application. RouteHandler : une classe qui contient le la classe annotee @Controller et la methode qui est annotee par @UrlMapping.

---
+++ src/com/etu003184/listener/AppInitializer.java
@@ -0,0 +1,58 @@
+package com.etu003184.listener;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+
+import java.io.File;
+import java.util.Map;
+
+import com.etu003184.util.ClassScanner;
+import com.etu003184.util.RouteHandler;
+
+@WebListener
+public class AppInitializer implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("=== Initialisation de l'application ===");
+
+        ServletContext context = sce.getServletContext();
+        ClassScanner scanner = new ClassScanner();
+
+        // scan classpath
+        String classesPath = context.getRealPath("/WEB-INF/classes");
+        try {
+            scanner.scanDirectory(new File(classesPath), "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Scanner tous les JAR dans WEB-INF/lib
+        // File libDir = new File(context.getRealPath("/WEB-INF/lib"));
+        // if (libDir.exists() && libDir.isDirectory()) {
+        //     for (File jar : libDir.listFiles((d, name) -> name.endsWith(".jar"))) {
+        //         scanner.scanJar(jar);
+        //     }
+        // }
+
+        Map<String, RouteHandler> routes = scanner.getRouteMap();
+        context.setAttribute("ROUTES", routes);
+
+        System.out.println("=== Routes enregistrées ===");
+        for (String url : routes.keySet()) {
+            RouteHandler handler = routes.get(url);
+            System.out.println("  " + url + " -> " +
+                    handler.getControllerClass().getName() + "." +
+                    handler.getMethod().getName());
+        }
+
+        System.out.println("=== Initialisation terminée ===");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("=== Application arrêtée ===");
+    }
+}
