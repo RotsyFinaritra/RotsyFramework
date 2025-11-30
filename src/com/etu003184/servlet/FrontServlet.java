29365e0119f01ea2cd0019820bef5d5674b23bf5
Randriamifidisoa Rotsy Finaritra <rotsyfinaritra0@gmail.com>
2025-11-30 18:52:40 +0300

    sprint 8
    Raha misy Map<String, Object> ny argument an'ny fonction dia fenoina amin'ny parametres an'ny requete.

--- src/com/etu003184/servlet/FrontServlet.java
+++ src/com/etu003184/servlet/FrontServlet.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 
@@ -178,6 +179,8 @@ public class FrontServlet extends HttpServlet {
                     args[0] = paramValue;
                 } else if (parameters[0].getType() == double.class || parameters[0].getType() == Double.class) {
                     args[0] = Double.parseDouble(paramValue);
+                } else if (GenericUtil.isMapStringObject(parameters[0])) {
+                    args[0] = GenericUtil.getMappedParameters(req);
                 }
 
                 Object result = GenericUtil.handleMethodWithParameters(controllerInstance, method, args, 1, req, resp);
